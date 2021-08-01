e0e722bd82a6c37aaa1faf8176341a6666efcc52
Morimura Sintaro <snoopy.850807@gmail.com>
2021-08-01 12:32:50 +0800

    testing

--- bootstrap/master_data.php
+++ bootstrap/master_data.php
@@ -6,6 +6,23 @@
  * @author RedSpider
  */
 
+define('USER_NOT_LOGINNED', 0);
+define('USER_LOGINNED', 1);
+
+define('API_RESULT_SUCCESS', 0);
+define('API_RESULT_FAILURE', 1);
+
+define('API_ERROR_NONE', 0);
+define('API_ERROR_RUNTIME', 1);
+define('API_ERROR_NO_DATA', 2);
+define('API_ERROR_INVALID_USER', 11);
+define('API_ERROR_INVALID_PASSWORD', 12);
+define('API_ERROR_BANNED_USER', 13);
+define('API_ERROR_ALREADY_LOGINNED', 14);
+define('API_ERROR_INVALID_LICENSE', 15);
+
+define('ACCESS_INTERVAL', 5 * 60);
+
 # Role Data
 define('USER_ROLE_ADMIN', 'admin');
 define('USER_ROLE_SUBSCRIBER', 'writer');
--- config/constant.php
+++ config/constant.php
@@ -3,19 +3,4 @@
 /////////////////////////////////////////////////////////////////////////////
 // API Constants
 /////////////////////////////////////////////////////////////////////////////
-define('USER_NOT_LOGINNED', 0);
-define('USER_LOGINNED', 1);
 
-define('API_RESULT_SUCCESS', 0);
-define('API_RESULT_FAILURE', 1);
-
-define('API_ERROR_NONE', 0);
-define('API_ERROR_RUNTIME', 1);
-define('API_ERROR_NO_DATA', 2);
-define('API_ERROR_INVALID_USER', 11);
-define('API_ERROR_INVALID_PASSWORD', 12);
-define('API_ERROR_BANNED_USER', 13);
-define('API_ERROR_ALREADY_LOGINNED', 14);
-define('API_ERROR_INVALID_LICENSE', 15);
-
-define('ACCESS_INTERVAL', 5 * 60);
