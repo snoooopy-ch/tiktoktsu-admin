a7c290ba1221b9022443383eed70e869ffe932cb
Morimura Sintaro <snoopy.850807@gmail.com>
2021-08-11 17:35:44 +0800

    replace logo and menu

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -72,13 +72,13 @@
                 <div class="navbar-container content">
                     <div class="navbar-collapse justify-content-end" id="navbar-mobile">
                         <div class="mr-auto float-left bookmark-wrapper d-flex align-items-center">
-                            <a class="nav navbar-nav" href="{{ route('dashboard') }}"><img
-                                    src="{{ cAsset('images/customer_1627208365.png') }}" width="50" /></a>
                             <ul class="nav navbar-nav d-lg-none">
                                 <li class="nav-item mobile-menu d-xl-none mr-auto"><a
                                         class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i
                                             class="ficon feather icon-menu"></i></a></li>
                             </ul>
+                            <a class="nav navbar-nav" href="{{ route('dashboard') }}"><img
+                                    src="{{ cAsset('images/customer_1627208365.png') }}" width="50" /></a>
                             <ul class="nav navbar-nav bookmark-icons d-lg-none">
                                 <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-todo.html"
                                         data-toggle="tooltip" data-placement="top" title="Todo"><i
